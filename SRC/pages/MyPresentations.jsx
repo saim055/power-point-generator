8d980aee3d008219290a8b17ee0722e2c13993bf
saim055 <saim@aladhwa-sch.net>
2026-02-15 00:11:12 +0400

    Update MyPresentations.jsx

--- SRC/pages/MyPresentations.jsx
+++ SRC/pages/MyPresentations.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useQuery, useMutation, useQueryClient } from '@tanstack/react-query';
-import { base44 } from "@/api/base44Client";
+import { base44 } from '../API/base44Client';
 import { Button } from "@/components/ui/button";
 import { Card, CardContent } from "@/components/ui/card";
 import { 
