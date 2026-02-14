939fcdb3ec4969c3edc394519448af012739264c
saim055 <saim@aladhwa-sch.net>
2026-02-14 23:51:26 +0400

    Update main.jsx

--- SRC/main.jsx
+++ SRC/main.jsx
@@ -1,1 +1,5 @@
-import '../SRC/main.jsx';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+
+ReactDOM.createRoot(document.getElementById('root')).render(<App />);
